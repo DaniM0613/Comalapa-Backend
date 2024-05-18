b4f181e6def64a369a1a15b30eabbd7e739afb88
GlendyT <glendytuyuc@gmail.com>
2024-05-17 20:48:16 -0600

    importar y exportar correctamente componentes y definier email como unique

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { connectDB } from './config/db.js'
 import foodRouter from './routes/foodRouter.js'
+import userRouter from './routes/userRouter.js'
 
 
 
@@ -20,6 +21,7 @@ connectDB();
 // api endpoints
 app.use("/api/food", foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user", userRouter)
 
 
 
